b2d2796acdb9b6fa6f71d7fbd6616d638776b221
imroot811 <84623228+imroot811@users.noreply.github.com>
2024-04-28 21:56:43 +0530

    receipt.py

--- receipt.py
+++ receipt.py
@@ -20,8 +20,8 @@ from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet
 
-# Data which we are going to display as tables
-DATA = [#2d matrix where each list is a row and each element in the list is column
+
+DATA = [
     ["Date", "Name", "Subscription", "Price (Rs.)"],
     ["27/04/2024","YouTube Premium","12 Months"," 1,290.00/-"],
     ["27/04/2024", "Spotify Premium", "2 Months", "199.00/-"],
@@ -32,40 +32,26 @@ DATA = [#2d matrix where each list is a row and each element in the list is colu
     ["Total", "", "", "2,737.00/-"],
 ]
 
-# Creating a Base Document Template of page size A4
 pdf = SimpleDocTemplate("receipt.pdf", pagesize=A4)
 
-# Standard stylesheet defined within ReportLab itself
 styles = getSampleStyleSheet()
 
-# Fetching the style of Top-level heading (Heading1)
 title_style = styles["Heading1"]
 
-# 0: left, 1: center, 2: right
 title_style.alignment = 1
 
-# Creating the paragraph with
-# the heading text and passing the styles of it
-title = Paragraph("Payment Receipt", title_style)
 
-# Creates a Table Style object and in it,
-# defines the styles row wise
-# the tuples which look like coordinates
-# are nothing but rows and columns
+title = Paragraph("Payment Receipt", title_style)
 style1= TableStyle(
     [
-        ("BOX", (0, 0), (-1, -1), 2, colors.black), #formats the entire table with black border
-        ("GRID", (0, 0), (6, 6), 1, colors.black), #creates grid
-        ("BACKGROUND", (0, 0), (3, 0), colors.gray), # sets bg to gray
-        ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke), #textcolor for first row
-        ("ALIGN", (0, 0), (-1, -1), "CENTER"),# aligns entire text to center
-        ("BACKGROUND", (0, 1), (-1, -1), colors.beige),#formats entire table with beige bg
+        ("BOX", (0, 0), (-1, -1), 2, colors.black),
+        ("GRID", (0, 0), (6, 6), 1, colors.black),
+        ("BACKGROUND", (0, 0), (3, 0), colors.gray),
+        ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
+        ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+        ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
     ]
 )
 
-# Creates a table object and passes the style to it
 table = Table(DATA, style=style1)
-
-# Final step which builds the
-# actual PDF putting together all the elements
 pdf.build([title, table])
